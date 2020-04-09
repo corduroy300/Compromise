38b1a8026ff3bf2306af693bc82f42df6db0e566
corduroy300 <opptagon@gmail.com>
2020-04-09 03:43:33 -0400

    Initial Commit

---
+++ Compromise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Compromise
+//
+//  Created by Matthew Tran on 4/9/20.
+//  Copyright © 2020 Capper Tech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
